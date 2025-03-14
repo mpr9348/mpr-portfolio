96455ca38005d4a315974d8324680d3d4866a551
BuildTools <unconfigured@null.spigotmc.org>
2025-03-14 10:57:19 -0700

    fixes for mobile

--- src/mobile.js
+++ src/mobile.js
@@ -61,7 +61,7 @@ const rowSpeeds = [0.01, -0.015, 0.01];
 const totalImagesPerRow = gridCols * duplicateFactor;
 
 
-const shadowTexture = textureLoader.load('./shadow.png');
+const shadowTexture = textureLoader.load('shadow.png');
 
 
 const rowGroups = [];
@@ -79,7 +79,7 @@ for (let i = 1; i <= gridRows * gridCols; i++) {
   const col = (i - 1) % gridCols;
 
   textureLoader.load(
-    `/homepage images/${i}.png`,
+    `homepage images/${i}.png`,
     (texture) => {
       texture.encoding = THREE.sRGBEncoding;
 
@@ -251,7 +251,7 @@ window.addEventListener("resize", resizeCanvas2);
 
 
 const images = [
-  '/512art/dream.png',         
+  '512art/dream.png',         
   '/512art/majoras mask.png', 
   '/512art/desert.png',       
   '/512art/glass universe.png', 
